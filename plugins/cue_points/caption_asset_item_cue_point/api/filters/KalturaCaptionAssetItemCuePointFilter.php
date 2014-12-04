49513344cf43d491dd9578b3d6f435327e7f850a
yossipapi <yossi.papiashvili@kaltura.com>
2014-12-04 12:28:23 +0200

    Update captions cue filter map between objects

--- plugins/cue_points/caption_asset_item_cue_point/api/filters/KalturaCaptionAssetItemCuePointFilter.php
+++ plugins/cue_points/caption_asset_item_cue_point/api/filters/KalturaCaptionAssetItemCuePointFilter.php
@@ -5,4 +5,15 @@
  */
 class KalturaCaptionAssetItemCuePointFilter extends KalturaCaptionAssetItemCuePointBaseFilter
 {
+	static private $map_between_objects = array
+	(
+		"contentLike" => "_like_text",
+		"contentMultiLikeOr" => "_mlikeor_text",
+		"contentMultiLikeAnd" => "_mlikeand_text",
+	);
+	
+	public function getMapBetweenObjects()
+	{
+		return array_merge(parent::getMapBetweenObjects(), self::$map_between_objects);
+	}
 }
